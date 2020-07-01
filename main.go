01cd46340f20e1306eda10373f451b1cf5f1434e
Michael Kennedy <michael.kennedy@sony.com>
2020-07-01 19:49:39 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("hello go!"))
+	})
+
+	http.ListenAndServe(":8000",nil)
+}
